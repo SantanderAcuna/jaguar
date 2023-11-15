01714cc757e01e13c94374e510eae73dfdbead4e
Jose acuna <joseacunapolo@gmail.com>
2023-11-15 09:13:28 -0500

    pagina vivero hermano

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1326-1700056994.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-79308a83 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-79308a83 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-79308a83 .swiper-button-next","prevEl":".uagb-block-79308a83 .swiper-button-prev"}}	);
+});
+
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-0763d0b3', {"layout":"circle","heading":"Micro-viveros","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":120,"totalNumber":120,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":200,"circleStokeSize":9,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-ec2e82a6', {"layout":"circle","heading":"\u00c1rboles sembrados","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":62000,"totalNumber":62000,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":220,"circleStokeSize":9,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-263418dc', {"layout":"circle","heading":"Especies nativas en nuestra red","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":75,"totalNumber":80,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":200,"circleStokeSize":9,"isFrontend":true} );
+});
+ });
